a5c7b186ee1d5e2773820792d7c6a9cb35d2e9ff
= <=>
2025-01-03 20:57:21 +0100

    yes

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
+description = '''This Shit should Change the Nicknames of people on the server with an ez command'''
+
 load_dotenv()
 intents = discord.Intents.default()
-intents.voice_states=True
-intents.message_content=True
-client = discord.Client(command_prefix='$', intents=intents)
+client = commands.Bot(command_prefix='$', description=description, intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.command(name='change')
+async def change(ctx, string: str):
+    """Change Nicknames"""
+    await print('test')
 
 TOKEN = os.getenv('TOKEN')
 client.run(TOKEN)
