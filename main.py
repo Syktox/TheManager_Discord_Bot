90cd388a9352e9a9c90df2cb84f75e264de8c711
= <=>
2025-01-11 21:36:44 +0100

    updated and finished changedNick and removeAllNicknames

--- main.py
+++ main.py
@@ -17,28 +17,29 @@ bot = commands.Bot(command_prefix='$', description=description, intents=intents)
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-@bot.command(name='change')
-@commands.has_permissions(manage_nicknames=True)
-async def change(ctx, member: discord.Member, new_nickname: str):
-    try:
-        old_nickname = member.nick or member.name  # Fallback to username if no nickname is set
-        await member.edit(nick=new_nickname)
-        await ctx.send(f"Changed nickname for {old_nickname} to {new_nickname}.")
-    except discord.Forbidden:
-        await ctx.send("I do not have permission to change this member's nickname.")
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
 
-@bot.command('changeAll')
-async def changeAll(ctx, occurrences: str , new_nickname: str):
+@bot.command('changeNick')
+async def changeNick(ctx, member: discord.Member, nick: str):
+    try:
+        await member.edit(nick=nick)
+        await ctx.send(f"Changed nickname of {member.mention}")
+    except discord.Forbidden:
+        print(f"Can't change the nickname of {member.name} : {member.nick}")
+
+@bot.command('removeAllNicknames', pass_content=True)
+async def removeAllNicknames(ctx):
     for server in bot.guilds:
         for member in server.members:
-            member_list.append(member)
-            if member.nick == "Maike4Seven ":
-                member.nick = None
-                await ctx.send(f"changed {member.name}s nickname back")
-            await ctx.send(f"{member.id} : {member} : {member.nick} : {member.name}")
-        
+            await ctx.send(f"Current user: {member.name} : {member.nick} : {member.id}")
+            try:
+                if member.nick:
+                    await member.edit(nick=None)
+                    await ctx.send(f"Removed nickname from: {member.name}")
+            except discord.Forbidden:
+                print(f"Can't change the nickname of {member.name} : {member.nick}")
+            except discord.HTTPException as e:
+                print(f"Error : {e}")
+             
 
 TOKEN = os.getenv('TOKEN')
 bot.run(TOKEN)
---
+++ readMe.md
@@ -0,0 +1,9 @@
+# The Manager
+
+
+### Commands
+Every command needs to be used with a $
+
+
+- removeAllNicknames : Removes every nickname on the server
+- changeNick @member (needs to be mentioned) new_nickname : Changes the nickname of the mentioned member to the new_nickname
