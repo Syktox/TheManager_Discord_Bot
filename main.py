d6caeffecc40469ebe8bd313aaf2890918080d94
= <=>
2025-01-13 13:37:29 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
-import datetime
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
-from commands import *
-import events
+import datetime
+import bot_commands
+import bot_events
 
 description = '''This Shit should change the nicknames of people on the server with an command'''
 
 load_dotenv()
 intents = discord.Intents.all()
-intents.members = True
-intents.voice_states=True
-intents.message_content=True
 
 bot = commands.Bot(command_prefix='$', description=description, intents=intents)
-events.bot = bot
-commands.bot = bot
+bot_events.bot = bot
+bot_commands.bot = bot
 
-for event_name in dir(events):
-    if event_name.startswith("on_"):  # Nur Funktionen mit "on_" berücksichtigen
-        event_func = getattr(events, event_name)
+for event_name in dir(bot_events):
+    if event_name.startswith("on_"):
+        event_func = getattr(bot_events, event_name)
         bot.event(event_func)
 
-bot.add_command(changeNickname)
-bot.add_command(removeAllNicknames)
-bot.add_command(removeAllNicknamesExceptRole)
-bot.add_command(switch_join_message)
-bot.add_command(switch_leave_message)
+for command_name in dir(bot_commands):
+    command_func = getattr(bot_commands, command_name)
+    if isinstance(command_func, commands.Command):
+        bot.add_command(command_func)
 
 TOKEN = os.getenv('TOKEN')
 bot.run(TOKEN)
